c2cd10f6fda55673d32869f1b69e2f650fc66c66
OmarJaff <me@omarjaff.com>
2024-03-06 21:16:59 +0300

    Added neccessary DB columns

--- database/migrations/2024_03_05_173050_create_products_table.php
+++ database/migrations/2024_03_05_173050_create_products_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Image;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -13,6 +14,10 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('description')->nullable();
+            $table->unsignedInteger('price');
+            $table->foreignIdFor(Image::class);
             $table->timestamps();
         });
     }
