f429e03922f8d6e146cf967cf3cb0371852985f1
OmarJaff <me@omarjaff.com>
2024-03-06 22:12:07 +0300

    Adjusted factory to make the first image as featured

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,7 +3,9 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Image;
 use App\Models\Product;
+use Illuminate\Database\Eloquent\Factories\Sequence;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,7 +16,7 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
       Product::factory(4)
-          ->hasVariants(5)->hasImages(3)
+          ->hasVariants(5)->has(Image::factory(3)->sequence(fn(Sequence $sequence) => ['featured' => $sequence->index == 0] ))
           ->create();
     }
 }
