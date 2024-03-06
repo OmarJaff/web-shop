087b143db8de4d9b66cb28d823866ea497944f09
OmarJaff <me@omarjaff.com>
2024-03-06 22:05:42 +0300

    Added factory definition

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,6 +13,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-      Product::factory(4)->create();
+      Product::factory(4)
+          ->hasVariants(5)->hasImages(3)
+          ->create();
     }
 }
