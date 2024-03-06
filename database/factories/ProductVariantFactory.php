61c4028a091a76d360e5d7e60bc0b0cf342a896e
OmarJaff <me@omarjaff.com>
2024-03-06 21:42:07 +0300

    Definded factory's default states

--- database/factories/ProductVariantFactory.php
+++ database/factories/ProductVariantFactory.php
@@ -17,7 +17,8 @@ class ProductVariantFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            'color' => $this->faker->randomElement(['Red', 'Gray', 'Blue', 'White']),
+            'size' => $this->faker->randomElement(['S', 'M', 'L', 'XL', 'XXL'])
         ];
     }
 }
--- database/migrations/2024_03_05_174602_create_images_table.php
+++ database/migrations/2024_03_05_174602_create_images_table.php
@@ -16,6 +16,7 @@ return new class extends Migration
             $table->id();
             $table->foreignIdFor(Product::class);
             $table->string('path');
+            $table->boolean('featured')->default(false);
             $table->timestamps();
         });
     }
