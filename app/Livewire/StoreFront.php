23658f7fcb0a9d4e1f863e7c390f7774c4fb1ab8
OmarJaff <me@omarjaff.com>
2024-03-08 14:43:33 +0300

    Used computed properties to fetch products list

--- app/Livewire/StoreFront.php
+++ app/Livewire/StoreFront.php
@@ -2,10 +2,18 @@
 
 namespace App\Livewire;
 
+use App\Models\Product;
+use Livewire\Attributes\Computed;
 use Livewire\Component;
 
 class StoreFront extends Component
 {
+    #[computed]
+    public function products ()
+    {
+        return Product::query()->get();
+    }
+
     public function render()
     {
         return view('livewire.store-front');
