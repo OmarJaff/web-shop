07af37ff247e90b86d080b7f23db3dcd8afc41bf
OmarJaff <me@omarjaff.com>
2024-03-08 15:02:18 +0300

    added the store layout and design

--- resources/views/livewire/store-front.blade.php
+++ resources/views/livewire/store-front.blade.php
@@ -1,5 +1,33 @@
 <div>
-      @foreach($this->products as $product )
-          <h1>{{$product->name}}</h1>
-      @endforeach
+
+    <div class="bg-white">
+        <div class="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
+            <h2 class="sr-only">Products</h2>
+
+            <div class="grid grid-cols-1 gap-y-4 sm:grid-cols-2 sm:gap-x-6 sm:gap-y-10 lg:grid-cols-3 lg:gap-x-8">
+                @foreach($this->products as $product)
+                <div class="group relative flex flex-col overflow-hidden rounded-lg border border-gray-200 bg-white">
+                    <div class="aspect-h-4 aspect-w-3 bg-gray-200 sm:aspect-none group-hover:opacity-75 sm:h-96">
+                        <img src="{{$product->images}}" alt="Eight shirts arranged on table in black, olive, grey, blue, white, red, mustard, and green." class="h-full w-full object-cover object-center sm:h-full sm:w-full">
+                    </div>
+                    <div class="flex flex-1 flex-col space-y-2 p-4">
+                        <h3 class="text-sm font-medium text-gray-900">
+                            <a href="#">
+                                <span aria-hidden="true" class="absolute inset-0"></span>
+                                {{$product->name}}
+                            </a>
+                        </h3>
+                        <p class="text-sm text-gray-500">{{$product->description}}</p>
+                        <div class="flex flex-1 flex-col justify-end">
+
+                            <p class="text-base font-medium text-gray-900">{{$product->price}} $</p>
+                        </div>
+                    </div>
+                </div>
+                @endforeach
+
+            </div>
+        </div>
+    </div>
+
 </div>
